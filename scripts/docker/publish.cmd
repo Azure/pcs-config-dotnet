9edc741798338c834a09966d3b510e109204677b
Xiangzhi Sheng <xisheng@microsoft.com>
2017-09-19 13:03:48 +0800

    Fix broken docker publish script (#48)

--- scripts/docker/publish.cmd
+++ scripts/docker/publish.cmd
@@ -1,7 +1,7 @@
 @ECHO off & setlocal enableextensions enabledelayedexpansion
 
 :: Note: use lowercase names for the Docker images
-SET DOCKER_IMAGE="azureiotpcs/pcs-uiconfig-dotnet"
+SET DOCKER_IMAGE="azureiotpcs/pcs-ui-config-dotnet"
 
 :: strlen("\scripts\docker\") => 16
 SET APP_HOME=%~dp0
