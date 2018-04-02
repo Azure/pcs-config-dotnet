ea227d1c8b9abc416c802da1b4d243a4df8b9550
Isaac <jojosep@microsoft.com>
2018-04-02 13:26:53 -0700

    Docker Temp changes

--- scripts/docker/build.cmd
+++ scripts/docker/build.cmd
@@ -1,7 +1,7 @@
 @ECHO off & setlocal enableextensions enabledelayedexpansion
 
 :: Note: use lowercase names for the Docker images
-SET DOCKER_IMAGE=azureiotpcs/pcs-config-dotnet
+SET DOCKER_IMAGE=isaac73/pcs-config-dotnet
 :: "testing" is the latest dev build, usually matching the code in the "master" branch
 SET DOCKER_TAG=%DOCKER_IMAGE%:testing
 
